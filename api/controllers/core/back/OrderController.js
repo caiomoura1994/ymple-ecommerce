3acd3ab403ca2087285045eed2f3af547847f4d2
fabienthetis <fabien.thetis@gmail.com>
2018-03-18 14:13:50 +0100

    add details to order page

--- api/controllers/core/back/OrderController.js
+++ api/controllers/core/back/OrderController.js
@@ -5,121 +5,164 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 var async = require('async');
-var pathTemplateBackCore =  sails.config.globals.templatePathBackCore;
+var pathTemplateBackCore = sails.config.globals.templatePathBackCore;
 var pathToService = '../../../services/core/';
 
 var CoreReadDbService = require(pathToService + 'back/CoreReadDbService');
 
 module.exports = {
-	
 
-  /**
-   * `OrderController.manage()`
-   */
 
-  item: function (req, res, id) {
-    let result = {
-      admin: req.session.user
-    };
+    /**
+     * `OrderController.item()`
+     *
+     *
+     *
+     * Will get the customer details + the shipping address + invoice address + product list + order item
+     *
+     */
 
-    //result.page = page;
+    item: function (req, res, id) {
+        let result = {
+            admin: req.session.user
+        };
 
-    async.waterfall([
 
-      function GetEditProduct (next) {
+        console.log('OrderController - item - req.session', req.session);
 
-        let orderId = id;
 
-        //var orderId = req.params.id;
-        CoreReadDbService.getOrderItem(orderId).then(function (order) {
+        async.waterfall([
 
-          console.log ( 'OrderController - item - order',order );
 
-          //if (err) next (err);
-          result.data = order;
+            function (callback) {
 
-          return next(null);
+                let orderId = id;
+
+                //var orderId = req.params.id;
+                CoreReadDbService.getOrderItem(orderId).then(function (order) {
+
+                    console.log('OrderController - item - order', order);
+
+                    let email = "test@test.com"
+
+                    if (order && order[0]) {
+                        result.data = order[0];
+                    } else {
+                        result.data = null;
+                    }
+                    callback(null, email);
+                });
+
+            },
+
+
+            // need to add the customer informations to data
+            function (arg1, callback) {
+
+
+                //console.log ( ' OrderController - next ', next);
+
+                let email = arg1 ;
+
+                CoreReadDbService.getUserItemByEmail(email).then(function (user) {
+
+
+                    console.log ( 'orderController - user ', user );
+
+                    if ( user ){
+                    result.data.user = user;
+                    }
+                    callback (null, email);
+
+                })}
+
+
+
+
+
+
+
+        ], function (err) {
+            if (err) return res.serverError(err);
+
+            console.log ( 'OrderController - result.data', result.data);
+
+            result.templateToInclude = 'yes';
+            result.pathToInclude = '../order/item.ejs';
+
+            return res.view(pathTemplateBackCore + 'commun-back/main.ejs', result);
         });
-      }
-    ], function (err) {
-      if (err) return res.serverError(err);
+    },
 
-      result.templateToInclude  = 'yes';
-      result.pathToInclude = '../order/item.ejs';
+    manage: function (req, res) {
+        var result = {
+            admin: req.session.user
+        };
+        var skip = 0;
+        var page = 1;
 
-      return res.view(pathTemplateBackCore +'commun-back/main.ejs', result);
-    });
-  },
-
-  manage: function (req, res) {
-    var result = {
-      admin: req.session.user
-    };
-    var skip = 0;
-    var page = 1;
-
-    if ( req.query.hasOwnProperty('page') ){
-      skip = (req.query.page - 1) * 10;
-      page = req.query.page;
-    }
-
-    /*var queryOptions = {
-      where: {},
-      skip: skip,
-      limit: 10,
-      sort: 'createdAt DESC'
-    };*/
-
-    result.page = page;
-
-    async.waterfall([
-
-      function GetOrders (next) {
-
-        CoreReadDbService.getOrderList().then(function (data1) {
-
-          console.log ( 'back OrderController - manage - data1', data1);
-
-          result.orders = data1;
-
-          return next(null);
-        });
-      },
-
-      function GetEditProduct (next) {
-        if ( !req.params.hasOwnProperty('id') ) {
-          return next(null);
+        if (req.query.hasOwnProperty('page')) {
+            skip = (req.query.page - 1) * 10;
+            page = req.query.page;
         }
 
-        var orderId = req.params.id;
-        CoreReadDbService.getOrderItem(orderId).then(function (order) {
+        /*var queryOptions = {
+         where: {},
+         skip: skip,
+         limit: 10,
+         sort: 'createdAt DESC'
+         };*/
+
+        result.page = page;
+
+        async.waterfall([
+
+            function GetOrders(next) {
+
+                CoreReadDbService.getOrderList().then(function (data1) {
+
+                    console.log('back OrderController - manage - data1', data1);
+
+                    result.orders = data1;
+
+                    return next(null);
+                });
+            },
+
+            function GetEditProduct(next) {
+                if (!req.params.hasOwnProperty('id')) {
+                    return next(null);
+                }
+
+                var orderId = req.params.id;
+                CoreReadDbService.getOrderItem(orderId).then(function (order) {
 
 
-          //if (err) next (err);
-          result.edit = order;
+                    //if (err) next (err);
+                    result.edit = order;
 
-          return next(null);
+                    return next(null);
+                });
+            }
+        ], function (err) {
+            if (err) return res.serverError(err);
+
+
+            result.templateToInclude = 'yes';
+
+            result.pathToInclude = '../order/list.ejs';
+
+            return res.view(pathTemplateBackCore + 'commun-back/main.ejs', result);
         });
-      }
-    ], function (err) {
-      if (err) return res.serverError(err);
+    },
 
-
-      result.templateToInclude  = 'yes';
-
-      result.pathToInclude = '../order/list.ejs';
-
-      return res.view(pathTemplateBackCore +'commun-back/main.ejs', result);
-    });
-  },
-
-  /**
-   * `OrderController.list()`
-   */
-  list: function (req, res) {
-    return res.json({
-      todo: 'list() is not implemented yet!'
-    });
-  }
+    /**
+     * `OrderController.list()`
+     */
+    list: function (req, res) {
+        return res.json({
+            todo: 'list() is not implemented yet!'
+        });
+    }
 };
 
